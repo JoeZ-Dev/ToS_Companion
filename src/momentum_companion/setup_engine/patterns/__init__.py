1262f7c0a241cfa69cab81a17a6fbdc081dc8249
JoeZ-Dev <joe.zammataro@gmail.com>
2026-09-22 04:40:00 -0400

    pattern engine: add __init__.py

---
+++ src/momentum_companion/setup_engine/patterns/__init__.py
@@ -0,0 +1,4 @@
+from momentum_companion.setup_engine.patterns.ascending_triangle import detect_ascending_triangle
+from momentum_companion.setup_engine.patterns.micro_pullback import detect_micro_pullback
+
+__all__ = ["detect_ascending_triangle", "detect_micro_pullback"]
