6492f7ca9b6f51cfdccfed60540d3d5a82cc332a
JoeZ-Dev <joe.zammataro@gmail.com>
2026-09-25 16:13:49 -0400

    feat: map security status and borrow fields from Schwab L1

--- src/momentum_companion/clients/stream_mapping.py
+++ src/momentum_companion/clients/stream_mapping.py
@@ -43,12 +43,37 @@ class LevelOneCache:
                 "last": fields.get("3"),
                 "bid_size": fields.get("4"),
                 "ask_size": fields.get("5"),
-                "last_size": None,
+                "last_size": fields.get("9"),
                 "volume": fields.get("8"),
+                "previous_close": fields.get("12"),
+                "security_status": fields.get("32"),
+                "htb_quantity": fields.get("46"),
+                "htb_rate": fields.get("47"),
+                "hard_to_borrow": fields.get("48"),
+                "shortable": fields.get("49"),
             }
+            numeric_fields = {
+                "bid", "ask", "last", "bid_size", "ask_size", "last_size",
+                "volume", "previous_close", "htb_quantity", "htb_rate",
+            }
+            boolean_fields = {"hard_to_borrow", "shortable"}
             for key, val in mapping.items():
-                if val is not None:
+                if val is None:
+                    continue
+                if key in numeric_fields:
                     sym_cache[key] = float(val)
+                elif key in boolean_fields:
+                    if isinstance(val, bool):
+                        sym_cache[key] = val
+                    else:
+                        try:
+                            parsed = int(val)
+                        except (TypeError, ValueError):
+                            continue
+                        if parsed >= 0:
+                            sym_cache[key] = bool(parsed)
+                else:
+                    sym_cache[key] = str(val)
 
             if not all(k in sym_cache for k in REQUIRED_FIELDS):
                 continue
@@ -64,6 +89,12 @@ class LevelOneCache:
                     ask_size=sym_cache.get("ask_size"),
                     last_size=sym_cache.get("last_size"),
                     volume=sym_cache.get("volume"),
+                    previous_close=sym_cache.get("previous_close"),
+                    security_status=sym_cache.get("security_status"),
+                    htb_quantity=sym_cache.get("htb_quantity"),
+                    htb_rate=sym_cache.get("htb_rate"),
+                    hard_to_borrow=sym_cache.get("hard_to_borrow"),
+                    shortable=sym_cache.get("shortable"),
                     source_ts_type="QUOTE_TS",
                     raw_source="SCHWAB_STREAM",
                 )
